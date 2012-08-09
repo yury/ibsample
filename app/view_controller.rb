d3b06314afbc4ad5602b9aa2ae9f2aba40bba24c
yury <yury.korolev@gmail.com>
2012-08-09 15:46:45 +0400

    Update to latest ib gem

--- app/view_controller.rb
+++ app/view_controller.rb
@@ -1,14 +1,14 @@
 class ViewController < UIViewController
   extend IB
 
-  outlet_accessor :label, UILabel
+  outlet :label, UILabel
 
   def viewWillAppear animated
     @counter = 0
     label.text = "Click the button"
   end
 
-  def touch button
+  def touch
     @counter += 1
     label.text = "Touched #{@counter} times"
   end
